3e943de015c8bd1ade96591988f454b01af6e867
Nikolay56615 <75627798+Nikolay56615@users.noreply.github.com>
2024-09-16 11:55:39 +0700

    checkstyle

--- Task_1_1_2/src/test/java/ru/nsu/lebedev/MainTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/lebedev/MainTest.java
@@ -1,13 +1,20 @@
 package ru.nsu.lebedev;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 
 class MainTest {
